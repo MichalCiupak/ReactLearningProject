1de6613c9f56e0e373485a590eceb9b512bcb772
MichalCiupak <michalciupak.26@gmail.com>
2024-03-09 15:59:00 +0000

    Fix indentation in testing file

--- src/__tests__/interfaces.test.tsx
+++ src/__tests__/interfaces.test.tsx
@@ -1,99 +1,99 @@
 import '@testing-library/jest-dom';
 import { ICharacter, IEpisode, ILocation, ICharacterDetails, IEpisodeApiResponse, 
-ICharacterApiResponse, ICharacterDetailsApiResponse } from '../utils/interfaces';
+  ICharacterApiResponse, ICharacterDetailsApiResponse } from '../utils/interfaces';
 
 
 test('ICharacter interface', () => {
-const character: ICharacter = {
+  const character: ICharacter = {
     id: 1,
     name: 'Rick Sanchez',
     species: 'Human'
-};
+  };
 
-expect(character).toBeDefined();
-expect(character.id).toBe(1);
-expect(character.name).toBe('Rick Sanchez');
-expect(character.species).toBe('Human');
+  expect(character).toBeDefined();
+  expect(character.id).toBe(1);
+  expect(character.name).toBe('Rick Sanchez');
+  expect(character.species).toBe('Human');
 });
 
 
 test('IEpisode interface', () => {
-    const episode: IEpisode = {
-      id: 1,
-      episode: 'S04E01',
-      name: 'Edge of Tomorty: Rick, Die, Rickpeat',
-      air_date: '2017-12-02'
-    };
+  const episode: IEpisode = {
+    id: 1,
+    episode: 'S04E01',
+    name: 'Edge of Tomorty: Rick, Die, Rickpeat',
+    air_date: '2017-12-02'
+  };
 
-    expect(episode).toBeDefined();
-    expect(episode.id).toBe(1);
-    expect(episode.episode).toBe('S04E01');
-    expect(episode.name).toBe('Edge of Tomorty: Rick, Die, Rickpeat');
-    expect(episode.air_date).toBe('2017-12-02');
+  expect(episode).toBeDefined();
+  expect(episode.id).toBe(1);
+  expect(episode.episode).toBe('S04E01');
+  expect(episode.name).toBe('Edge of Tomorty: Rick, Die, Rickpeat');
+  expect(episode.air_date).toBe('2017-12-02');
 });
 
 
 test('ILocation interface', () => {
-    const location: ILocation = {
-      id: 1,
-      name: 'Earth'
-    };
+  const location: ILocation = {
+    id: 1,
+    name: 'Earth'
+  };
 
-    expect(location).toBeDefined();
-    expect(location.id).toBe(1);
-    expect(location.name).toBe('Earth');
+  expect(location).toBeDefined();
+  expect(location.id).toBe(1);
+  expect(location.name).toBe('Earth');
 });
 
 
 test('ICharacterDetails interface', () => {
-    const location: ILocation = {
-      id: 1,
-      name: 'Earth'
-    };
+  const location: ILocation = {
+    id: 1,
+    name: 'Earth'
+  };
 
-    const characterDetails: ICharacterDetails = {
-      id: 1,
-      name: 'Rick Sanchez',
-      species: 'Human',
-      status: 'Alive',
-      type: 'Human',
-      gender: 'Male',
-      origin: location,
-      location: location,
-      image: 'https://rickandmortyapi.com/rick_sanchez.jpeg'
-    };
+  const characterDetails: ICharacterDetails = {
+    id: 1,
+    name: 'Rick Sanchez',
+    species: 'Human',
+    status: 'Alive',
+    type: 'Human',
+    gender: 'Male',
+    origin: location,
+    location: location,
+    image: 'https://rickandmortyapi.com/rick_sanchez.jpeg'
+  };
 
-    expect(characterDetails).toBeDefined();
-    expect(characterDetails.id).toBe(1);
-    expect(characterDetails.name).toBe('Rick Sanchez');
-    expect(characterDetails.species).toBe('Human');
-    expect(characterDetails.status).toBe('Alive');
-    expect(characterDetails.type).toBe('Human');
-    expect(characterDetails.gender).toBe('Male');
-    expect(characterDetails.origin).toEqual(location);
-    expect(characterDetails.location).toEqual(location);
-    expect(characterDetails.image).toBe('https://rickandmortyapi.com/rick_sanchez.jpeg');
+  expect(characterDetails).toBeDefined();
+  expect(characterDetails.id).toBe(1);
+  expect(characterDetails.name).toBe('Rick Sanchez');
+  expect(characterDetails.species).toBe('Human');
+  expect(characterDetails.status).toBe('Alive');
+  expect(characterDetails.type).toBe('Human');
+  expect(characterDetails.gender).toBe('Male');
+  expect(characterDetails.origin).toEqual(location);
+  expect(characterDetails.location).toEqual(location);
+  expect(characterDetails.image).toBe('https://rickandmortyapi.com/rick_sanchez.jpeg');
 });
 
 
 test('IEpisodeApiResponse interface', () => {
   const episodeApiResponse: IEpisodeApiResponse = {
-      episodes: {
-          results: [
-              {
-                  id: 1,
-                  episode: 'S04E01',
-                  name: 'Edge of Tomorty: Rick, Die, Rickpeat',
-                  air_date: '2017-12-02'
-              },
-              {
-                  id: 2,
-                  episode: 'S04E02',
-                  name: 'The Old Man and the Seat',
-                  air_date: '2017-12-09'
-              }
-          ]
-      }
+    episodes: {
+      results: [
+        {
+          id: 1,
+          episode: 'S04E01',
+          name: 'Edge of Tomorty: Rick, Die, Rickpeat',
+          air_date: '2017-12-02'
+        },
+        {
+          id: 2,
+          episode: 'S04E02',
+          name: 'The Old Man and the Seat',
+          air_date: '2017-12-09'
+        }
+      ]
+    }
   };
 
   expect(episodeApiResponse).toBeDefined();
@@ -105,24 +105,24 @@ test('IEpisodeApiResponse interface', () => {
 
 test('ICharacterApiResponse interface', () => {
   const characterApiResponse: ICharacterApiResponse = {
-      episodes: {
-          results: [
-              {
-                  characters: [
-                      {
-                          id: 1,
-                          name: 'Rick Sanchez',
-                          species: 'Human'
-                      },
-                      {
-                          id: 2,
-                          name: 'Morty Smith',
-                          species: 'Human'
-                      }
-                  ]
-              }
+    episodes: {
+      results: [
+        {
+          characters: [
+            {
+              id: 1,
+              name: 'Rick Sanchez',
+              species: 'Human'
+            },
+            {
+              id: 2,
+              name: 'Morty Smith',
+              species: 'Human'
+            }
           ]
-      }
+        }
+      ]
+    }
   };
 
   expect(characterApiResponse).toBeDefined();
@@ -135,17 +135,17 @@ test('ICharacterApiResponse interface', () => {
 
 test('ICharacterDetailsApiResponse interface', () => {
   const characterDetailsApiResponse: ICharacterDetailsApiResponse = {
-      character: {
-          id: 1,
-          name: 'Rick Sanchez',
-          species: 'Human',
-          status: 'Alive',
-          type: 'Human',
-          gender: 'Male',
-          origin: { id: 1, name: 'Earth' },
-          location: { id: 1, name: 'Earth' },
-          image: 'https://rickandmortyapi.com/rick_sanchez.jpeg'
-      }
+    character: {
+      id: 1,
+      name: 'Rick Sanchez',
+      species: 'Human',
+      status: 'Alive',
+      type: 'Human',
+      gender: 'Male',
+      origin: { id: 1, name: 'Earth' },
+      location: { id: 1, name: 'Earth' },
+      image: 'https://rickandmortyapi.com/rick_sanchez.jpeg'
+    }
   };
 
   expect(characterDetailsApiResponse).toBeDefined();
